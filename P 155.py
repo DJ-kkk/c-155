4bcbacb3c1b7f9223636b379a5c1a409f4b50d26
DJ-kkk <129987327+DJ-kkk@users.noreply.github.com>
2024-05-15 17:16:24 +0100

    Add files via upload

---
+++ P 155.py
@@ -0,0 +1,27 @@
+from tkinter import *
+root =  Tk()
+
+root.title("Random Colours")
+root.geometry("1600x1000")
+
+
+
+
+
+dictionary["colour"][random_no]
+dictionary["colour"][random_no]
+
+
+["maroon1","lawn green","magenta2","purple1","springgreen2","chocolate1","deep pink","cyan"]
+
+
+random.radiant()
+
+
+
+btn = Button (root, text= " click me ", command = report)
+btn.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+
+
+root.mainloop()
+
